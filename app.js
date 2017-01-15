2adb924f1d3cca32c031b3f6d1e997c40a726efd
José Muñoz R <jozeepp@gmail.com>
2017-01-15 19:15:27 -0300

    se crea componente hijo ListaEmpleados>Empleados

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
+var empleados = [
+  { nombre: "Pepe", email: "pepe@correo.com", imagen: "foto1.png" },
+  { nombre: "Paco", email: "paco@correo.com", imagen: "foto2.png" },
+  { nombre: "Manolo", email: "manolo@correo.com", imagen: "foto3.png" }
+];
+
 class App extends React.Component {
   render() {
-    return (<h1>Componente de ejemplo </h1>);
+    return <ListaEmpleados empleados={empleados} />;
+  }
+}
+
+class Empleado extends React.Component {
+  render() {
+    return (
+      <li>
+        <img src={this.props.imagen} width="30" />
+        {this.props.nombre} - {this.props.email}
+      </li>
+    );
+  }
+}
+
+class ListaEmpleados extends React.Component {
+  renderEmpleado() {
+    return
+  }
+  render () {
+    return (
+      <ul>
+        {
+          this.props.empleados.map(empleado => {
+            return (
+              <Empleado
+                nombre = {empleado.nombre}
+                email = {empleado.email}
+                imagen = {empleado.imagen}
+              />
+            );
+          })
+        }
+      </ul>
+    );
   }
 }
 
